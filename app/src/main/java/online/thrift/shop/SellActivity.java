b2efcf3651f1d6924463d43ec37c042d510bc90b
Chal13W1zz <chaliewizz4@gmail.com>
2022-01-11 12:22:24 +0300

    add sell activity

---
+++ app/src/main/java/online/thrift/shop/SellActivity.java
@@ -0,0 +1,14 @@
+package online.thrift.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell);
+    }
+}
