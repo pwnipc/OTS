2ff7bfa4f821b87bfe74b88901fc2818d371abe3
echwaadams <adamsechwa18@gmail.com>
2022-01-11 18:51:35 +0300

    created user account acivity.

---
+++ app/src/main/java/online/thrift/shop/SignupActivity.java
@@ -0,0 +1,32 @@
+package online.thrift.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class SignupActivity extends AppCompatActivity {
+    //variables
+    Button callSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This line will hide the status bar from the screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_signup);
+
+        callSignUp = findViewById(R.id.login);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
